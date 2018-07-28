e0bdab0a9a3a3b63f83db372e8aa22e3dc813348
Alex Korovyansky <inbox@alexkorovyansky.com>
2018-07-28 20:01:16 +0600

    Read dummy data from Firestore database

--- app/src/main/java/org/mobilatorium/happydo/MainActivity.kt
+++ app/src/main/java/org/mobilatorium/happydo/MainActivity.kt
@@ -2,11 +2,30 @@ package org.mobilatorium.happydo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+
+        val docRef = db.collection("days").document("2018-07-28")
+        docRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document.exists()) {
+                    Log.d("main_activity", "DocumentSnapshot data: " + document.data)
+                } else {
+                    Log.d("main_activity", "No such document")
+                }
+            } else {
+                Log.d("main_activity", "get failed with ", task.exception)
+            }
+        }
     }
 }
