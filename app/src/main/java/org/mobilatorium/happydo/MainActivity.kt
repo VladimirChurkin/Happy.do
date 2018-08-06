60e1352a2f02a106ad911a7c1a26439c52a328a4
Roman Goltsov <romer533@mail.ru>
2018-08-06 16:33:49 +0600

    Fix feature #26
    Fixed feature #26

--- app/src/main/java/org/mobilatorium/happydo/MainActivity.kt
+++ app/src/main/java/org/mobilatorium/happydo/MainActivity.kt
@@ -6,15 +6,11 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
-<<<<<<< HEAD
 import kotlinx.android.synthetic.main.activity_main.*
-=======
 import kotlinx.android.synthetic.main.activity_main.addFeedCatToDoButton
 
 
 
-
->>>>>>> db4ac793dfcb0bc215cbaad7e3c365c32b034285
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,7 +26,6 @@ class MainActivity : AppCompatActivity() {
         addFeedCatToDoButton.setOnClickListener { _ ->
             addNewTaskToDate(randomTasks.shuffled()[0], "2018-12-31")
         }
-<<<<<<< HEAD
 
         getTasks("2018-12-31")
 
@@ -48,8 +43,6 @@ class MainActivity : AppCompatActivity() {
                     }
                     textView.text = "$date \n ${snapshot?.documents?.map { "${it.get("action")} ${it.get("completed")} \n" }}"
                 })
-=======
->>>>>>> db4ac793dfcb0bc215cbaad7e3c365c32b034285
     }
 
     private fun printTasksFromDateToLogcat(date: String) {
