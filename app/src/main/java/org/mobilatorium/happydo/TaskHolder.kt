eb926325c561887ed19b4813bc7df7143e7462ed
Vladimir <vladimir_churkin2002@mail.ru>
2018-08-19 21:32:09 +0600

    Пока ничего еще не работает, но код есть
    Попытка реализовать удаление тасков

--- app/src/main/java/org/mobilatorium/happydo/TaskHolder.kt
+++ app/src/main/java/org/mobilatorium/happydo/TaskHolder.kt
@@ -1,15 +1,36 @@
 package org.mobilatorium.happydo
 
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.CheckBox
-import kotlinx.android.synthetic.main.custom_list_view_item.view.action
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.custom_list_view_item.view.*
 
-
+@RequiresApi(Build.VERSION_CODES.O)
 class TaskHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val action: CheckBox = itemView.action
+    private val removeButton: Button = itemView.button_delete
 
     fun bind(task: Task) {
         action.text = task.action
+        removeButton.setOnClickListener {
+            val documentId:String = this.itemView?.tag.toString()
+            MainActivity().db.collection("tasks")
+                    .document(documentId)
+                    .delete()
+                    .addOnSuccessListener { Log.d("task_holder", "successfully removed!") }
+                    .addOnFailureListener { e ->
+                        Log.w("task_holder", "Error removing task", e)
+                    }
+            MainActivity().adapter.notifyDataSetChanged()
+        }
     }
+
+//    private fun deleteTask(){
+//       
+//    }
 }
