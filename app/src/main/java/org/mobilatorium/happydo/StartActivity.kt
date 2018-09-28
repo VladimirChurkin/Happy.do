ad78852419f9d093dc4d05b6c69fb03a357439bd
Vladimir <vladimir_churkin2002@mail.ru>
2018-09-28 19:27:08 +0600

    hello world firebase auth

--- app/src/main/java/org/mobilatorium/happydo/StartActivity.kt
+++ app/src/main/java/org/mobilatorium/happydo/StartActivity.kt
@@ -5,8 +5,17 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_start.*
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.util.*
+import java.util.Arrays.asList
+
+
 
 class StartActivity : AppCompatActivity() {
+    private val RC_SIGN_IN = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +24,40 @@ class StartActivity : AppCompatActivity() {
 
         //логинимся
         button_login.setOnClickListener {
-            startActivity(Intent(this@StartActivity, MainActivity::class.java))
-        }
+            val providers = Arrays.asList(
+                    AuthUI.IdpConfig.EmailBuilder().build(),
+                    AuthUI.IdpConfig.PhoneBuilder().build(),
+                    AuthUI.IdpConfig.GoogleBuilder().build())
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN)        }
 
         //переходим на сайт с инфой
         button_info.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://happy.do")))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(Intent(this, MainActivity::class.java))
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
         }
     }
 }
