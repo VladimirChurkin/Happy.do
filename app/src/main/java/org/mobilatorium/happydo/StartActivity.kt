9265a5ab4f3eda14be33a7325aa279dafb45b801
romer533 <romer5333@gmail.com>
2018-10-20 21:59:45 +0600

    Small change
    Delete ";" and false string

--- app/src/main/java/org/mobilatorium/happydo/StartActivity.kt
+++ app/src/main/java/org/mobilatorium/happydo/StartActivity.kt
@@ -1,16 +1,13 @@
 package org.mobilatorium.happydo
 
 import android.content.Intent
-import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_start.*
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import java.util.*
-import java.util.Arrays.asList
 
 
 
@@ -39,18 +36,14 @@ class StartActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data);
+            val response = IdpResponse.fromResultIntent(data)
 
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
-                val user = FirebaseAuth.getInstance().getCurrentUser();
+                val user = FirebaseAuth.getInstance().getCurrentUser()
                 startActivity(Intent(this, MainActivity::class.java))
                 // ...
             } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
             }
         }
     }
