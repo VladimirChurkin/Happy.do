dac16557fdbcb5edb299218d2a36b2c10807ac6d
Vladimir <vladimir_churkin2002@mail.ru>
2018-10-28 23:01:12 +0600

    small change

--- app/src/main/java/org/mobilatorium/happydo/StartActivity.kt
+++ app/src/main/java/org/mobilatorium/happydo/StartActivity.kt
@@ -8,12 +8,13 @@ import kotlinx.android.synthetic.main.activity_start.*
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import java.util.*
+import java.util.Arrays.asList
 
 
 
 class StartActivity : AppCompatActivity() {
-
     private val RC_SIGN_IN = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,12 +24,6 @@ class StartActivity : AppCompatActivity() {
 
         //логинимся
         button_login.setOnClickListener {
-            startActivity(Intent(this@StartActivity, MainActivity::class.java))
-        }
-
-        //переходим на сайт с инфой
-        button_info.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://happy.do")))
             val providers = Arrays.asList(
                     AuthUI.IdpConfig.EmailBuilder().build())
 
@@ -44,14 +39,18 @@ class StartActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
+            val response = IdpResponse.fromResultIntent(data);
 
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
-                val user = FirebaseAuth.getInstance().getCurrentUser()
+                val user = FirebaseAuth.getInstance().getCurrentUser();
                 startActivity(Intent(this, MainActivity::class.java))
                 // ...
             } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
             }
         }
     }
